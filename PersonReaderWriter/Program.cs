71ede2e69b309974fe11d91ee68cf73ba2f8d967
Your Name <you@example.com>
2020-06-03 09:34:54 +0200

    added person reader writer

---
+++ PersonReaderWriter/Program.cs
@@ -0,0 +1,71 @@
+﻿using PersonReaderWriter.models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PersonReaderWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> personList = new List<Person>();
+            /*
+             * read all person lines to a string Array
+             * personLinesFromFile[0] ==> "Hans;43;Dornbirn"
+             */
+            string[] personLinesFromFile = File.ReadAllLines("C:\\CC\\persons.txt");
+            
+            /*
+             * iterate each person - line
+             */
+            foreach(var personLine in personLinesFromFile)
+            {
+                /*
+                 * split lines to columns
+                 * personColumns[0] ==> "Hans"
+                 * personColumns[1] ==> "43"
+                 */
+                string[] personColumns = personLine.Split(";");
+
+                /*
+                 * initializes a new field age with the value of tryParse from the input
+                 */
+                int.TryParse(personColumns[1], out var age);
+
+                /*
+                 * creates a person object and set the job
+                 * property with an "if - clause"
+                 */
+                Person person = new Person()
+                {
+                    Name = personColumns[0],
+                    Age = age,
+                    City = personColumns[2],
+                    Job = personColumns[0] == "Hans" || personColumns[0] == "Helga" ? "Coder" : "Officer"
+                };
+
+                personList.Add(person);
+            }
+
+            List<string> outputList = new List<string>();
+
+            /*
+             * iterates the modified person objects
+             */
+            foreach(var person in personList)
+            {
+                /*
+                 * format the output string for the new file
+                 */
+                outputList.Add($"{person.Name};{person.Age};{person.City};{person.Job}");
+                //outputList.Add(person.Name + ";" + person.Age + ";" + person.City + ";" + person.Job);
+            }
+
+            /*
+             * write all output strings
+             */
+            File.WriteAllLines("C:\\CC\\persons1.txt", outputList);
+        }
+    }
+}
---
+++ PersonReaderWriter/models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonReaderWriter.models
+{
+    /// <summary>
+    /// person class
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// persons full name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// persons age
+        /// </summary>
+        public int Age { get; set; }
+        /// <summary>
+        /// persons hometown
+        /// </summary>
+        public string City { get; set; }
+        /// <summary>
+        /// persons current job
+        /// </summary>
+        public string Job { get; set; }
+    }
+}
